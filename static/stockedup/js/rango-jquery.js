cd987cda5634c0589635d2923db366b39c777fea
Greg Tucker <2191051t@student.gla.ac.uk>
2017-03-14 01:20:39 +0000

    Added view toggle for front page

--- static/stockedup/js/rango-jquery.js
+++ static/stockedup/js/rango-jquery.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $("#login").hide();
+    $("#signup").show();
+    $("a.signup").click( function(event) {
+    $("#login").hide();
+    $("#signup").show();
+    });
+    $("a.login").click( function(event) {
+    $("#login").show();
+    $("#signup").hide();
+    });
 });
