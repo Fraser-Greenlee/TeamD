a119fe971cde01a789e64aad578dc4266a3223f5
Greg Tucker <2191051t@student.gla.ac.uk>
2017-03-14 16:38:41 +0000

    Created Models for database

--- static/stockedup/js/rango-jquery.js
+++ static/stockedup/js/rango-jquery.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
-    $("#login").hide();
-    $("#signup").show();
+    //Settings
+    var inFocusTextColor = "#fff"
+    var outFocusTextColor = "rgba(255, 255, 255, 0.41)"
+
+    // Initialisation
+
+    // When signup text is clicked show signup form
     $("a.signup").click( function(event) {
-    $("#login").hide();
-    $("#signup").show();
+        $("#login").hide();
+        $("#signup").show();
+        $(this).css('color', inFocusTextColor);
+        $("a.login").css('color', outFocusTextColor);
     });
+    // When login text is clicked show login form
     $("a.login").click( function(event) {
-    $("#login").show();
-    $("#signup").hide();
+        $("#login").show();
+        $("#signup").hide();
+        $(this).css('color', inFocusTextColor);
+        $("a.signup").css('color', outFocusTextColor);
     });
 });
--- stockedup/admin.py
+++ stockedup/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from stockedup.models import Supplier, User, Item
 # Register your models here.
+admin.site.register(Supplier)
+admin.site.register(Item)
