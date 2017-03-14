10e93f35e969b8d4230ef8399472c619c8266c14
Fraser <fraser.greenlee@icloud.com>
2017-03-14 21:42:01 +0000

    sample data for stocks

--- stockedup/views.py
+++ stockedup/views.py
@@ -4,4 +4,12 @@ def index(request):
 	return render(request, 'stockedup/index.html')
 
 def stock(request):
-	return render(request, 'stockedup/stock.html')
+	# sample data
+	return render(request, 'stockedup/stock.html', context={
+		'stock':[{'name':'Salad','from':'Harry Farms','kgmo':1.3,'amount':5,'ppkg':2}],
+		'orders':[
+			{
+				'date':'Fri 26 Nov','total':26,'orders':[{'name':'Salad','from':'Harry Farms','cost':30,'amount':10}]
+			}
+		]
+	})
