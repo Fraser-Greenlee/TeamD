1a3010e765f2765ac254bf288baa26c7399679d3
“Fraser <2192722g@student.gla.ac.uk>
2017-03-14 20:52:05 +0000

    signup/login select

--- static/stockedup/js/rango-jquery.js
+++ static/stockedup/js/rango-jquery.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){
-    //Settings
-    var inFocusTextColor = "#fff"
-    var outFocusTextColor = "rgba(255, 255, 255, 0.41)"
-
-    // Initialisation
-
     // When signup text is clicked show signup form
     $("a.signup").click( function(event) {
-        $("#login").hide();
-        $("#signup").show();
-        $(this).css('color', inFocusTextColor);
-        $("a.login").css('color', outFocusTextColor);
+			if ($("#authbox").hasClass("login")) {
+				$("#authbox").addClass('signup').removeClass('login');
+			}
     });
     // When login text is clicked show login form
     $("a.login").click( function(event) {
-        $("#login").show();
-        $("#signup").hide();
-        $(this).css('color', inFocusTextColor);
-        $("a.signup").css('color', outFocusTextColor);
+			if ($("#authbox").hasClass("signup")) {
+				$("#authbox").addClass('login').removeClass('signup');
+			}
     });
 });
