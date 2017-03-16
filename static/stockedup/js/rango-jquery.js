301c76c8efdf408644ae748b7210f1fba77927a1
Fraser <fraser.greenlee@icloud.com>
2017-03-16 18:13:17 +0000

    started the stock edit

--- static/stockedup/js/rango-jquery.js
+++ static/stockedup/js/rango-jquery.js
@@ -11,4 +11,48 @@ $(document).ready(function(){
 				$("#authbox").addClass('login').removeClass('signup');
 			}
     });
+		EditTrigger();
 });
+
+function EditTrigger() {
+	$("#currentStock a.edit").click( function(event) {
+		$("#currentStock ul:not(#additem)").each(function(i) {
+			newHtml = '';
+			$(this).children("span").each(function(i) {
+				newHtml += '<input type="text" name="'+$(this).attr("data-type")+'" placeholder="'+$(this).attr("data-type")+'" value="'+$(this).text()+'">';
+			});
+			newHtml += '<a class="removelink">Remove</a>';
+			$(this).html(newHtml);
+		});
+		RemoveTriggers();
+		$("#currentStock > span").html('<h1>Stock</h1><a class="save">Save</a><a class="cancel">Cancel</a>');
+		SaveCancelTriggers();
+		$("#currentStock").addClass("edit");
+	});
+}
+
+function RemoveTriggers() {
+	$("#currentStock a.removelink").click( function(event) {
+		$(this).parent().addClass("willRemove");
+	});
+}
+
+function SaveCancelTriggers() {
+	$("#currentStock a.cancel").click( function(event) {
+		$("#currentStock ul:not(#additem)").each(function(i) {
+			newHtml = '';
+			$(this).children("input").each(function(i) {
+				newHtml += '<span data-type="'+$(this).attr('name')+'">'+$(this).attr('value')+'</span>';
+			});
+			$(this).html(newHtml);
+			$(this).removeClass("willRemove");
+		});
+		$("#currentStock > span").html('<h1>Stock</h1><a class="edit">Edit</a>');
+		EditTrigger();
+		$("#currentStock").removeClass("edit");
+	});
+	// save then display input values
+	$("#currentStock a.save").click( function(event) {
+		console.log('not implimented');
+	});
+}
--- stockedup/views.py
+++ stockedup/views.py
@@ -6,7 +6,7 @@ def index(request):
 def stock(request):
 	# sample data
 	return render(request, 'stockedup/stock.html', context={
-		'stock':[{'name':'Salad','from':'Harry Farms','kgmo':1.3,'amount':5,'ppkg':2}],
+		'stock':[{'name':'Salad','from':'Harry Farms','kgmo':1.3,'amount':5,'ppkg':2},{'name':'Carrots','from':'Harry Farms','kgmo':3.1,'amount':5,'ppkg':2}],
 		'orders':[
 			{
 				'date':'Fri 26 Nov','total':26,'orders':[{'name':'Salad','from':'Harry Farms','cost':30,'amount':10}]
