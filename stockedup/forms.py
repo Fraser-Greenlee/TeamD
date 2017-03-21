705848b0feaf98b03424abec482d8ec537b74d82
thePeachtree <kieranpetrie@outlook.com>
2017-03-21 21:12:45 +0000

    no longer breaks on signup when passwords are the same

--- stockedup/forms.py
+++ stockedup/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from registration.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class MyRegForm(RegistrationForm):
-    username = forms.CharField(max_length=254, required=False, widget=forms.HiddenInput())
 
     def clean_email(self):
         email = self.cleaned_data['email']
         self.cleaned_data['username'] = email
         return email
 
-    error_messages = {}
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        # remove username
+        self.fields.pop('username')
